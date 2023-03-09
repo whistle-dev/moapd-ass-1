4e915f0000543ce980bc6892724592e2f578200c
DavidDHansson <hansson.d.david@gmail.com>
2023-03-09 10:56:17 +0100

    Kdoc

--- app/src/main/java/dk/itu/moapd/scootersharing/rasni/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/rasni/MainActivity.kt
@@ -3,15 +3,18 @@ package dk.itu.moapd.scootersharing.rasni
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
+/**
+ * The main activity used on startup of app
+ *
+ * This class uses the starts with the fragment MainFragment
+ *
+ */
 class MainActivity : AppCompatActivity() {
 
-    // A set of private constants used in this class .
-    companion object {
-        private val TAG = MainActivity::class.qualifiedName
-        lateinit var ridesDB: RidesDB
-    }
-
+    /**
+     * override of onCreate
+     * Sets up the contentView and sets up the MainFragment with a transaction
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
