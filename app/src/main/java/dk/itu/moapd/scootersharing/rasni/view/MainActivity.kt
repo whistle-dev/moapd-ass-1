39cf52378bf2d075608726a2b9456bbd13eda1a9
Rasmus Nielsen <rasni@itu.dk>
2023-04-18 11:11:37 +0200

    fucked appbar

--- app/src/main/java/dk/itu/moapd/scootersharing/rasni/view/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/rasni/view/MainActivity.kt
@@ -2,10 +2,13 @@ package dk.itu.moapd.scootersharing.rasni.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import dk.itu.moapd.scootersharing.rasni.R
 import dk.itu.moapd.scootersharing.rasni.controller.MainFragment
+import dk.itu.moapd.scootersharing.rasni.controller.StartRideFragment
+import dk.itu.moapd.scootersharing.rasni.controller.UpdateRideFragment
 import dk.itu.moapd.scootersharing.rasni.controller.WelcomeFragment
 
 /**
@@ -26,8 +29,27 @@ class MainActivity : AppCompatActivity() {
         FirebaseApp.initializeApp(this)
         FirebaseAuth.getInstance().setLanguageCode("en")
 
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.menu_start_ride -> StartRideFragment()
+                R.id.menu_update_ride -> UpdateRideFragment()
+                R.id.menu_profile ->  MainFragment()
+                //R.id.navigation_history -> HistoryFragment()
+                else -> null
+            }
+            if (fragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit()
+                true
+            } else {
+                false
+            }
+        }
+
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, WelcomeFragment())
+            .replace(R.id.fragment_container, MainFragment())
             .commit()
     }
 }
